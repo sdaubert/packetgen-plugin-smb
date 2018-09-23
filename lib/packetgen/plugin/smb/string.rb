6b3097d05e00f600804cf8deaa9e00ed2fef23de
Sylvain Daubert <sylvain.daubert@laposte.net>
2018-09-23 21:05:34 +0200

    SMB::String: add support for ASCII-encoded strings.

--- lib/packetgen/plugin/smb/string.rb
+++ lib/packetgen/plugin/smb/string.rb
@@ -11,12 +11,24 @@ module PacketGen::Plugin
     # @author Sylvain Daubert
     # @since 0.1.0
     class String < PacketGen::Types::CString
+      # @param [Boolean] unicode
+      attr_writer :unicode
+
       # @param [Boolean, Proc] is string UTF-16 encoded?
       # @param [Hash] options
       # @option options [Integer] :static_length set a static length for this string
+      # @option options [Boolean] :unicode If +true+, string is encoded as a UTF-16
+      #    unicode string. If +false+, string is encode in ASCII. Defaults to +true+.
       def initialize(options={})
         super
-        self.encode!('UTF-16LE')
+        @unicode = options.key?(:unicode) ? options[:unicode] : true
+        self.encode!('UTF-16LE') if @unicode
+        self.encode!('ASCII-8BIT') unless @unicode
+      end
+
+      # @return [Boolean]
+      def unicode?
+        @unicode
       end
 
       # @param [::String] str
@@ -28,24 +40,32 @@ module PacketGen::Plugin
                when Encoding::BINARY
                  binidx = nil
                  0.step(to: str.size, by: 2) do |i|
-                   binidx = i if str[i, 2] == "\x00\x00"
+                   binidx = i if str[i, 2] == binary_terminator
                  end
                  s = if binidx.nil?
                        str
                      else
                        str[0, binidx]
                      end
-                 s.force_encoding('UTF-16LE')
-               when Encoding::UTF_16LE
-                 str
+                 s.force_encoding(self_encoding)
                else
-                 str.encode('UTF-16LE')
+                 str.encode(self_encoding)
                end
         str2 = str2[0, @static_length / 2] if @static_length.is_a? Integer
-        idx = str2.index(+"\x00".encode('UTF-16LE'))
+        idx = str2.index(+"\x00".encode(self_encoding))
         str2 = str2[0, idx] unless idx.nil?
         self.replace str2
         self
+      end
+
+      private
+
+      def self_encoding
+        @unicode ? Encoding::UTF_16LE : Encoding:: ASCII_8BIT
+      end
+
+      def binary_terminator
+        @unicode ? "\x00\x00" : "\x00"
       end
     end
   end
